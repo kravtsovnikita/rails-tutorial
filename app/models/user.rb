03eebad0464a85b5bdd4ce09f28e50e2721abfe8
Nikita Kravtsov <kravtsovn327@gmail.com>
2025-01-21 20:14:29 +0300

    validator for the user's

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_many :microposts
+
+  # Validator for the user's name
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+
+  # Validator for the user's email
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { minimum: 10, maximum: 50 }, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 end
